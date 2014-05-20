d77e8854b8261d40fd70c76f9393bbf5c0a4765e
luma2906 <luma2906@gmail.com>
2014-05-19 21:35:58 -0500

    movimiento del mapa
    se agragar las teclas de desplazamiento en el mapa

--- sources/principal.py
+++ sources/principal.py
@@ -3,10 +3,14 @@ from pygame.locals import *
 import sys
 import fondo
 from fondo import *
-ALTO= 1000
-ANCHO= 1000
+ALTO= 600   #Alto de la pantalla
+ANCHO= 1000 #Ancho de la pantalla
+V=30 #VELOCIDAD CON LA QUE SE VA MOVER EN EL MAPA
+
 
 def main():
+	X_Screen=0  #Variable de posicion x del subsurface a mostrar en la pantalla
+	Y_Screen=0  #Variable de posicion y del subsurface a mostrar en la pantalla
 	pygame.init()
 	#creamos la pantalla  mi screen	
 	pantalla = pygame.display.set_mode((ANCHO,ALTO)) 
@@ -14,18 +18,46 @@ def main():
 	#creo un objeto de tipo fondo
 	fondito=fondo()
 	#creo un buffer  que va contener mi mapa 
-	buffe=pygame.Surface((2000,2000))
+	buffe=pygame.Surface((4000,4000))
 	#creo una lista imagenes que va contener mis sprites
 	sprites=fondito.cargarSprites("final3.png")	
 	# dibujo mi buffer
 	fondito.DrawPantalla(buffe,sprites)
-	pantalla.blit(buffe.subsurface(400,0,800,800),(0,0))
+	pantalla.blit(buffe.subsurface(X_Screen,Y_Screen,ANCHO,ALTO),(0,0))
 	pygame.display.flip()
-	while True:		
+	
+	while True:	
+		tecla = pygame.key.get_pressed()
+		tecla = pygame.key.get_pressed()
+		pantalla.blit(buffe.subsurface(X_Screen,Y_Screen,ANCHO,ALTO),(0,0))
 		pygame.display.flip()	
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			if tecla[K_RIGHT]:				
+				if X_Screen == 3000:
+					print "fdgdsfh"
+				else:
+					X_Screen = X_Screen+V
+						
+				print "holaaa"
+			if tecla[K_DOWN]:				
+				if Y_Screen == 3000:
+					print "fdgdsfh"
+				else:
+					Y_Screen = Y_Screen+V
+						
+				print "holaaa"
+			if tecla[K_UP]:
+				if Y_Screen == 0:
+					print "fdgdsfh"
+				else:
+					Y_Screen = Y_Screen-V
+			if tecla[K_LEFT]:
+				if X_Screen == 0:
+					print "fdgdsfh"
+				else:
+					X_Screen = X_Screen-V
 		
 	
 
